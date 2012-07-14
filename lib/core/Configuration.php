3135daa1ca389a2d79ebb5f6833ddeb13815ff3e
Arief M Utama <arief.utama@gmail.com>
2012-07-14 23:55:32 +0700

    Refactor database class to pu2

--- lib/core/Configuration.php
+++ lib/core/Configuration.php
@@ -2,5 +2,28 @@
 namespace PetakUmpet;
 
 abstract class Configuration {
+  // real constants (all caps) dont change
+  const DBTYPE = 1;
+  const DBHOST = 2;
+  const DBNAME = 4;
+  const DBUSER = 8;
+  const DBCRED = 16;
+
+  // configuration constants
   const StartPage = 'Login/index';
+
+  public static function Database($index, $type)
+  {
+    // database configuration here
+    // for multiple connection just add more index
+    $config[0] = array(
+        self::DBTYPE => 'PostgreSQL',
+        self::DBHOST => 'localhost',
+        self::DBNAME => 'project_sehat',
+        self::DBUSER => 'dbsys',
+        self::DBCRED => '1',
+      );
+
+    return (isset ($config[$index][$type]) ? $config[$index][$type] : null);
+  }
 }
---
+++ lib/core/Database.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace PetakUmpet;
+
+class Database {
+
+  const FETCH_TYPE_ONE = 1;
+  const FETCH_TYPE_ROW = 2;
+  const FETCH_TYPE_ALL = 4;
+
+  private $db;
+  private $baseDatabaseObject;
+  private $errorInfo;
+
+  function __construct($dbConfigIndex=0)
+  {
+    $db_type = Configuration::Database($dbConfigIndex, Configuration::DBTYPE);
+    $db_host = Configuration::Database($dbConfigIndex, Configuration::DBHOST);
+    $db_user = Configuration::Database($dbConfigIndex, Configuration::DBUSER);
+    $db_cred = Configuration::Database($dbConfigIndex, Configuration::DBCRED);
+    $db_name = Configuration::Database($dbConfigIndex, Configuration::DBNAME);
+
+    $class_name = '\\PetakUmpet\\' .  $db_type . 'Database';
+
+    $this->baseDatabaseObject = new $class_name;
+
+    $this->db = $this->Connect($db_host, $db_name, $db_user, $db_cred);
+
+    if ($this->db === false) {
+      throw Exception;
+    }
+
+    $this->baseDatabaseObject->setDbo($this->db);
+  }
+
+  function Connect($host, $dbname, $user, $cred, $extra=null)
+  {
+    try {
+      $db = new \PDO($this->baseDatabaseObject->generateDSN($host, $dbname, $extra), $user, $cred);
+    } catch (Exception $e) {
+      echo 'Have you setup the database and update Configuration class?';
+
+      Logger::log('Database: connection failed, ' . $e);
+      return false;
+    }
+    return $db;
+  }
+
+  function Query($query, $trans=false)
+  {
+    Logger::log('Database: Query-> ' . $query);
+
+    if ($trans) {
+      try {
+        $this->db->beginTransaction();
+        $this->db->exec($query);
+        $this->db->commit();
+      } catch (Exception $e) {
+        $db->rollback();
+
+        Logger::log('Database: Query FAIL');
+        echo "Query Failed: " . $e->getMessage();
+        return false; 
+      }
+    } 
+
+    return $this->db->query($query, PDO::FETCH_ASSOC);
+  }
+
+  function preparedQuery($query, $params=array())
+  {
+    $st = $this->db->prepare($query);
+    $st->setFetchMode(PDO::FETCH_ASSOC);
+
+    Logger::log('Database: preparedQuery-> ' . $st->queryString);
+
+    if (!$st->execute($params)) {
+      $this->errorInfo = array('query' => $st->queryString, 'error' => $st->errorInfo());
+
+      Logger::log('Database: preparedQuery ERROR: ' . implode(' ', $st->errorInfo()));
+
+      return false;
+    }
+
+    return $st;
+  }
+
+  function QueryAndFetch($query, $fetch_type, $trans=false)
+  {
+    $st = $this->Query($query, $trans);
+
+    if ($st) {
+      if ($fetch_type == self::FETCH_TYPE_ONE) return $st->fetchColumn();
+      if ($fetch_type == self::FETCH_TYPE_ROW) return $st->fetchRow();
+      if ($fetch_type == self::FETCH_TYPE_ALL) return $st->fetchAll();
+    }
+    return false;
+  }
+
+  function getErrorText()
+  {
+    return $this->errorInfo['error'][2];
+  }
+
+}
+
