f045685fc6ee78cea6d20db3ae95a433f0b6a412
Arief M Utama <arief.utama@gmail.com>
2012-07-26 15:11:37 +0700

    Adding skeleton login form
    Signed-off-by: Arief M Utama <arief.utama@gmail.com>

---
+++ Skeleton/res/Form/LoginForm.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Form;
+
+use PetakUmpet\Form;
+use PetakUmpet\Validator;
+use PetakUmpet\Validator\Required;
+
+class LoginForm extends Form {
+
+  public function __construct($name='Login')
+  {
+    parent::__construct($name);
+
+    $this->add('text', 'name', array('required' => 'required', 'focus' => 'focus'));
+    $this->add('password', 'password', array('required' => true));
+
+    $this->addAction(new Form\Field\Submit('Login'));
+
+    $vld = new Validator;
+    $vld->set('name', new Required);
+    $vld->set('password', new Required);
+
+    $this->setValidator($vld);
+
+  } 
+
+}
