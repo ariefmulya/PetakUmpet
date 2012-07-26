d81e0d22ec7b79593792c8720a08ca04be072d64
Arief M Utama <arief.utama@gmail.com>
2012-07-26 15:15:12 +0700

    Add more missing files to skeleton
    Signed-off-by: Arief M Utama <arief.utama@gmail.com>

---
+++ Skeleton/res/View/Login/index.php
@@ -0,0 +1,5 @@
+<?php $T->snippet('flash-message'); ?>
+<div class="page-header">
+  <h3>Please login to access system</h3>
+</div>
+<?php echo $form; ?>
---
+++ Skeleton/res/View/User/profile.php
@@ -0,0 +1,7 @@
+<?php $T->snippet('flash_message'); ?>
+
+<div class="row-fluid">
+  <div class="span6">
+    <?php echo $form; ?>
+  </div>
+</div>
