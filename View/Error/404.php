23f47b58a7455d57fb6cd6aa030377329f43d5aa
Arief M Utama <arief.utama@gmail.com>
2012-08-15 15:23:21 +0700

    404 page
    Signed-off-by: Arief M Utama <arief.utama@gmail.com>

---
+++ View/Error/404.php
@@ -0,0 +1,12 @@
+<div class="hero-unit">
+  <h1>404 - Page Not Found</h1>
+  <p>
+    Sorry, the page that you are trying to access is not available. 
+  </p>
+  <p>Things you can do:
+    <ul>
+      <li>Check the address you entered in the browser</li>
+      <li>Check if the application requirements is fulfilled</li>
+    </ul>
+</div>
+
