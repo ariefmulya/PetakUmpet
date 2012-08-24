50f3b4bbf880a794c42df0237e761f42f1733c8f
Arief M Utama <arief.utama@gmail.com>
2012-08-25 03:38:53 +0700

    row-fluid is not good for side-forms
    Signed-off-by: Arief M Utama <arief.utama@gmail.com>

--- View/xCrud/index.php
+++ View/xCrud/index.php
@@ -1,27 +1,25 @@
 <div class="row">
-  <div class="row">
-    <div class="span5">
-      <h3>Cari data <?php echo ucwords(str_replace('_', ' ', $tableName)) ?></h3>
-      <?php echo $filterForm; ?>
-      <?php if ($readOnly === false) : ?>
-        <?php if ($inlineForm) : ?>
-          <a href="#" class="btn" 
-                onclick="$('#crud-form').load('<?php echo $editAction; ?>');">
-        <?php else : ?>
-          <a class="btn" href="<?php echo $editAction; ?>"> 
-        <?php endif ?>
-          Tambah Data</a>
+  <div class="span5">
+    <h3>Cari data <?php echo ucwords(str_replace('_', ' ', $tableName)) ?></h3>
+    <?php echo $filterForm; ?>
+    <?php if ($readOnly === false) : ?>
+      <?php if ($inlineForm) : ?>
+        <a href="#" class="btn" 
+              onclick="$('#crud-form').load('<?php echo $editAction; ?>');">
+      <?php else : ?>
+        <a class="btn" href="<?php echo $editAction; ?>"> 
       <?php endif ?>
-      <hr/>
-      <div id="pager" class="row-fluid">
-        <?php echo $pager; ?>
+        Tambah Data</a>
+    <?php endif ?>
+    <hr/>
+    <div id="pager" class="row-fluid">
+      <?php echo $pager; ?>
+    </div>
+  </div>
+  <?php if ($inlineForm) : ?>
+    <div class="span7">
+      <div id="crud-form">
       </div>
     </div>
-    <?php if ($inlineForm) : ?>
-      <div class="span7">
-        <div id="crud-form">
-        </div>
-      </div>
-    <?php endif ?>
-  </div>
+  <?php endif ?>
 </div>
