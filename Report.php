116e809ef5e1d57040378c4bb0388e950c5d6762
Arief M Utama <arief.utama@gmail.com>
2012-08-27 20:37:19 +0700

    Add more setter-getter
    Signed-off-by: Arief M Utama <arief.utama@gmail.com>

--- Report.php
+++ Report.php
@@ -28,6 +28,17 @@ class Report {
   public function getReportData() { return $this->reportData; }
   public function getFooter()     { return $this->footer; }
 
+  public function getHeaderKey($key)     { if (isset($this->header[$key])) return $this->header[$key]; }
+  public function getReportDataKey($key) { if (isset($this->reportData[$key])) return $this->reportData[$key]; }
+  public function getFooterKey($key)     { if (isset($this->footer[$key])) return $this->footer[$key]; }
+
+  public function setTitle($value)      { $this->title = $value; }
+  public function setHeader($value)     { $this->header = $value; }
+  public function setColumns($value)    { $this->columns = $value; }
+  public function setReportData($value) { $this->reportData = $value; }
+  public function setFooter($value)     { $this->footer = $value; }
+  public function setFormatter($value)  { $this->formatter = $value; }
+
   public function __toString()
   {
     $cname = '\\PetakUmpet\\Report\\' . $this->formatter;
