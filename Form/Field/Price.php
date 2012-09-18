45f33f06330165a527390f0dceeec06ab8f2aef6
Arief M Utama <arief.utama@gmail.com>
2012-09-18 19:05:08 +0700

    Add price field type
    Signed-off-by: Arief M Utama <arief.utama@gmail.com>

---
+++ Form/Field/Price.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace PetakUmpet\Form\Field;
+
+class Price extends BaseField {
+
+  private $currencyTag;
+  private $currency;
+
+  public function __construct($name=null, $extra=null, $label=null, $id=null)
+  {
+    parent::__construct($name, $extra, $label, $id);
+    $this->setType('text');
+
+    $this->currencyTag = 'Rp.';
+    $this->currency = 'IDR';
+    $this->setAttribute('style', 'text-align: right;');
+    $this->setAttribute('class', 'input-medium');
+  }
+
+  public function setCurrencyTag($value)
+  {
+    $this->currencyTag = $value;
+  }
+
+  public function setCurrency($value)
+  {
+    $this->currency = $value;
+  }
+
+  public function __toString()
+  {
+    $s  = '<div class="input-prepend">';
+    $s .= '<span class="add-on">' . $this->currencyTag . '</span>';
+    $s .= parent::__toString();
+    $s .= '</div>';
+
+    // extra script for datepicker field
+    $s .= '<script type="text/javascript">$(\'#'.$this->getAttribute('id').'\').priceFormat({prefix: ""});</script>';
+    return $s;
+  }
+
+}
