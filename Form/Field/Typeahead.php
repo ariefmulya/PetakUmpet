310b9256e9c26b073f7d5800266b7b084afc7d80
Arief M Utama <arief.utama@gmail.com>
2012-09-19 02:12:20 +0700

    Add typeahead field
    Signed-off-by: Arief M Utama <arief.utama@gmail.com>

---
+++ Form/Field/Typeahead.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace PetakUmpet\Form\Field;
+
+class Typeahead extends BaseField {
+
+  private $url;
+
+  public function __construct($name=null, $extra=null, $label=null, $id=null)
+  {
+    parent::__construct($name, $extra, $label, $id);
+    $this->setType('text');
+    $this->setAttribute('autocomplete', 'off');
+    $this->url = '#';
+  }
+
+  public function setSourceUrl($url)
+  {
+    $this->url = $url;
+  }
+
+  public function __toString()
+  {
+    $s = parent::__toString();
+    // extra script for datepicker field
+    $s .= '<script type="text/javascript">$(\'#'.$this->getAttribute('id').'\').typeahead( 
+              { 
+                source: function(query, process) { 
+                  return $.get("'.$this->url.'", {query: query}, 
+                    function (data) { 
+                      res = $.parseJSON(data);
+                      process(res);
+                    }
+                  );
+                }
+              }
+            );
+          </script>';
+    return $s;
+  }
+
+}
