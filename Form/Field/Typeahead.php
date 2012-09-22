e772b5f55f13b450de966c21e88253503371b930
Arief M Utama <arief.utama@gmail.com>
2012-09-22 21:54:56 +0700

    Updates to typeahead field
    Signed-off-by: Arief M Utama <arief.utama@gmail.com>

--- Form/Field/Typeahead.php
+++ Form/Field/Typeahead.php
@@ -6,6 +6,9 @@ class Typeahead extends BaseField {
 
   private $url;
 
+  private $actualName;
+  private $actualValue;
+
   public function __construct($name=null, $extra=null, $label=null, $id=null)
   {
     parent::__construct($name, $extra, $label, $id);
@@ -19,22 +22,35 @@ class Typeahead extends BaseField {
     $this->url = $url;
   }
 
+  public function setValue($value)
+  {
+    parent::setValue($value);
+    $this->setActualValue($value);
+  }
+
+  private function setActualValue($value)
+  {
+    $this->actualValue = $value;
+  }
+
   public function __toString()
   {
     $s = parent::__toString();
+
     // extra script for datepicker field
-    $s .= '<script type="text/javascript">$(\'#'.$this->getAttribute('id').'\').typeahead( 
-              { 
-                source: function(query, process) { 
-                  return $.get("'.$this->url.'", {query: query}, 
-                    function (data) { 
-                      res = $.parseJSON(data);
-                      process(res);
-                    }
-                  );
+    $s .= '<script type="text/javascript">
+              $(\'#'.$this->getAttribute('id').'\').typeahead( 
+                { 
+                  source: function(query, process) { 
+                    return $.get("'.$this->url.'", {query: query}, 
+                      function (data) { 
+                        res = $.parseJSON(data);
+                        process(res);
+                      }
+                    );
+                  }
                 }
-              }
-            );
+              );
           </script>';
     return $s;
   }
