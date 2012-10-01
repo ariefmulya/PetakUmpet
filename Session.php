d67ebecd9667099519e74893667416a51f5199ed
Arief M Utama <arief.utama@gmail.com>
2012-10-02 02:51:48 +0700

    Add ability to remove key from session
    Signed-off-by: Arief M Utama <arief.utama@gmail.com>

--- Session.php
+++ Session.php
@@ -44,6 +44,12 @@ class Session {
     $_SESSION[$name] = $value;
   }
 
+  public function remove($name)
+  {
+    $_SESSION[$name] = null;
+    unset($_SESSION[$name]);
+  }  
+
   public function getOrSet($name, $value=null)
   {
     if (isset($_SESSION[$name])) {
