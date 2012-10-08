0282984c159b830fa853f382ec1854f379e7aaf6
Arief M Utama <arief.utama@gmail.com>
2012-10-08 12:01:24 +0700

    Add chain ability in select
    Signed-off-by: Arief M Utama <arief.utama@gmail.com>

--- Form/Field/Select.php
+++ Form/Field/Select.php
@@ -7,6 +7,9 @@ class Select extends BaseField {
 
   private $options;
 
+  private $chainTarget;
+  private $chainUrl;
+
   public function __construct($name=null, $extra=null, $label=null, $id=null)
   {
     parent::__construct($name, $extra, $label, $id);
@@ -15,6 +18,9 @@ class Select extends BaseField {
     $this->endTag = '</select>';
     $this->useInnerValue = true;
     $this->useOptions = true;
+    $this->options = array();
+    $this->chainTarget = false;
+    $this->chainUrl = false;
   }
 
   public function setOptions(array $options)
@@ -27,11 +33,37 @@ class Select extends BaseField {
     $s = '';
     $s .= '<option value=""></option>';
 
-    foreach ($this->options as $k => $v) {
-      $t = $k == $this->getValue() ? 'selected' : '';
-      $s .= '<option value="'.$k.'" '.$t.'>'.$v.'</option>';
+    if (count($this->options) > 0) {
+      foreach ($this->options as $k => $v) {
+        $t = $k == $this->getValue() ? 'selected' : '';
+        $s .= '<option value="'.$k.'" '.$t.'>'.$v.'</option>';
+      }
     }
     return $s;
   }
 
+  public function setChainTarget($target, $url)
+  {
+    $this->chainTarget = $target;
+    $this->chainUrl = $url;
+  }
+
+  public function __toString()
+  {
+    $s  = parent::__toString();
+
+    if ($this->chainTarget !== false && $this->chainTarget != '') {
+      $id = $this->getAttribute('id');
+      $targetId = $this->chainTarget;
+      $targetUrl = $this->chainUrl;
+
+      $s .= "<script type='text/javascript'>
+              $(document).ready(function() { 
+                  $('#$id').selectChain({ target: $('#$targetId'), url: '$targetUrl', type: 'post' });
+              });
+            </script>";
+    }
+
+    return $s;
+  }
 }
