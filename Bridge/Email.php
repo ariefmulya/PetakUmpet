57fadd1da8a3d512f39761e9b880eac818af846f
Arief M Utama <arief.utama@gmail.com>
2012-12-18 01:02:22 +0700

    Adding bridge class, email using swift mailer
    Signed-off-by: Arief M Utama <arief.utama@gmail.com>

---
+++ Bridge/Email.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace PetakUmpet\Bridge;
+use PetakUmpet\Singleton;
+
+require_once (PU_DIR . DS . 'lib' . DS . 'SwiftMailer' . DS . 'swift_required.php');
+
+abstract class Email {
+
+  static public function send($subject, $to, $body, $type='text/plain')
+  {
+    $config = Singleton::acquire('\\PetakUmpet\\Config'); 
+
+    $emailConf = $config->getEmailConfig();
+    extract($emailConf);
+
+    $transport = \Swift_SmtpTransport::newInstance($server, $port)->setUsername($username)->setPassword($password);
+    $mailer    = \Swift_Mailer::newInstance($transport);
+    $mailer->from = $from;
+
+    $message = \Swift_Message::newInstance($subject)->setFrom($mailer->from)->setTo($to)->setBody($body, $type);
+
+    return $mailer->send($message);
+  }
+
+}
