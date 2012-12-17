dd321c73eb74c95ee47b4168e69fed9ccc99eabb
Ahmad Furqon <ahmad.furqon@gmail.com>
2012-12-18 00:48:34 +0700

    nicedit wysiwyg textarea

--- Form/Field/Wysiwyg.php
+++ Form/Field/Wysiwyg.php
@@ -9,7 +9,7 @@ class Wysiwyg extends Textarea {
   public function __construct($name=null, $extra=null, $label=null, $id=null)
   {
     parent::__construct($name, $extra, $label, $id);
-    $this->setAttribute('class', 'wysiwyg');
+    // $this->setAttribute('class', 'wysiwyg');
     $this->explanation = '';
   }
 
@@ -20,7 +20,23 @@ class Wysiwyg extends Textarea {
 
   public function __toString()
   {
-    $s = parent::__toString();
+    $js = '<script type="text/javascript">
+            bkLib.onDomLoaded(function() {
+              new nicEditor({';
+    // feature added here                
+    $js .= "uploadURI : '../../app/cargo/Lib/nicUpload.php', ";
+    $js .= "iconsPath:'../img/nicEditorIcons.gif',";
+    $js .= "fullPanel: true,";
+
+    $js .= "})";
+
+    $js .= ".panelInstance('".$this->getAttribute('id')."');";
+    $js .= "});";
+
+    $js .= "</script>";
+
+    // $s = $this->js;
+    $s = $js.parent::__toString();
     $s .= $this->explanation;
     
     return $s;
